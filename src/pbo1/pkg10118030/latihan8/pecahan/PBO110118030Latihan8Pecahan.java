0985a427f30767ef9e8045ac631c123acabe98a4
arisulistio37 <arisulistio37@gmail.com>
2019-09-28 17:24:04 +0700

    Initial commit

---
+++ src/pbo1/pkg10118030/latihan8/pecahan/PBO110118030Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118030.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ */
+public class PBO110118030Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : "+f);
+        System.out.println("Double : "+d);
+        
+    }
+    
+}
